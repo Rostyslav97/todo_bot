a41c1713738454ade07e8c118bc8bde8c23d5c9d
Rostyslav <malanyukrostik@gmail.com>
2022-05-31 12:27:25 +0300

    add todo, edit start

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import telebot
+from models import User, Todo
 
 
 bot = telebot.TeleBot('5396285529:AAGODUcC6QAr34VbeCACbFfoOcgI2T_Lf5Q')
@@ -6,7 +8,15 @@ bot = telebot.TeleBot('5396285529:AAGODUcC6QAr34VbeCACbFfoOcgI2T_Lf5Q')
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
+    if not User.select().where(User.chat_id==message.chat.id):
+        User.create(chat_id=message.chat.id) 
     bot.send_message(message.chat.id, f"Hello {message.chat.first_name} {message.chat.last_name}!")
 
 
-bot.infinity_polling() 
+@bot.message_handler(content_types=['text'])
+def create_todo_handler(message):
+    user = User.get(User.chat_id==message.chat.id)
+    Todo.create(task=message.text, is_done=False, date=datetime.today(), user=user)
+    bot.send_message(message.chat.id, "Your todo was saved")
+
+bot.infinity_polling()  
