6bb12f3fcfc2be57cd7dc67df0c947106afcfce1
Rostyslav <malanyukrostik@gmail.com>
2022-05-31 12:55:44 +0300

    get_todo_list

--- bot.py
+++ bot.py
@@ -13,10 +13,23 @@ def start_handler(message):
     bot.send_message(message.chat.id, f"Hello {message.chat.first_name} {message.chat.last_name}!")
 
 
+@bot.message_handler(commands=['today', 't'])
+def get_todo_list(message):
+    user = User.get(User.chat_id == message.chat.id)
+    todos = Todo.select().where(Todo.user == user, Todo.date == datetime.today())
+    message_text = []
+
+    for todo in todos:
+        message_text.append(f"<b>{todo.task}</b>\n")
+
+    bot.send_message(message.chat.id, "".join(message_text), parse_mode='HTML')
+
+
 @bot.message_handler(content_types=['text'])
 def create_todo_handler(message):
     user = User.get(User.chat_id==message.chat.id)
     Todo.create(task=message.text, is_done=False, date=datetime.today(), user=user)
     bot.send_message(message.chat.id, "Your todo was saved")
 
-bot.infinity_polling()  
+
+bot.infinity_polling()
